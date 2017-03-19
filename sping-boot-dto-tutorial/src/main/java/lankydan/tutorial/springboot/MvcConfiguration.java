1df8b8ee899c8eda862d3c6d91ad66f96901f34d
lankydan <danknewton@hotmail.com>
2017-03-19 18:38:20 +0000

    Returing DTOs from a Rest Controller in Spring Boot
    Returing DTOs from a Rest Controller in Spring Boot

---
+++ sping-boot-dto-tutorial/src/main/java/lankydan/tutorial/springboot/MvcConfiguration.java
@@ -0,0 +1,9 @@
+package lankydan.tutorial.springboot;
+
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfiguration {
+}
---
+++ sping-boot-dto-tutorial/src/main/java/lankydan/tutorial/springboot/PersonRestController.java
@@ -0,0 +1,47 @@
+package lankydan.tutorial.springboot;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class PersonRestController {
+
+  private static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
+  @RequestMapping("/getPersonDTO")
+  public PersonDTO getPersonDTO(@RequestParam(value = "personDTO") String jsonPersonDTO)
+      throws IOException {
+    return getPersonDTOFromJson(jsonPersonDTO);
+  }
+
+  private PersonDTO getPersonDTOFromJson(final String jsonPersonDTO) throws IOException {
+    return new ObjectMapper()
+        .setDateFormat(simpleDateFormat)
+        .readValue(jsonPersonDTO, PersonDTO.class);
+  }
+
+  @RequestMapping("/getPersonDTOList")
+  public List<PersonDTO> getPersonDTOList(
+      @RequestParam(value = "personDTO") String jsonPersonDTO,
+      @RequestParam(value = "personDTO2") String jsonPersonDTO2)
+      throws IOException {
+    final PersonDTO personDTO = getPersonDTOFromJson(jsonPersonDTO);
+    final PersonDTO personDTO2 = getPersonDTOFromJson(jsonPersonDTO2);
+    return Arrays.asList(personDTO, personDTO2);
+  }
+
+  @RequestMapping("/getPeopleDTO")
+  public PeopleDTO getPeopleDTO(
+      @RequestParam(value = "personDTO") String jsonPersonDTO,
+      @RequestParam(value = "personDTO2") String jsonPersonDTO2)
+      throws IOException {
+    final PersonDTO personDTO = getPersonDTOFromJson(jsonPersonDTO);
+    final PersonDTO personDTO2 = getPersonDTOFromJson(jsonPersonDTO2);
+    return new PeopleDTO(Arrays.asList(personDTO, personDTO2));
+  }
+}
